29dae055d42a373ca86821e6f3181261fd1d07f1
chen <1748567982@qq.com>
2022-12-04 11:47:24 +0800

    第一次提交

---
+++ src/main/java/com/example/himicroservice/HiMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.himicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/himicroservice/HiMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.himicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
